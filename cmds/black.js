4f119fc21b5ab007e7d3349c4403586df27a66b6
RandomlyReaper18 <140783342+RandomlyReaper18@users.noreply.github.com>
2025-05-29 12:15:42 +0800

    Create black.js

---
+++ cmds/black.js
@@ -0,0 +1,35 @@
+const axios = require("axios");
+const { sendMessage } = require("../handles/message"); // Correctly import sendMessage
+
+module.exports = {
+  name: "black",
+  description: "BlackBox try",
+  role: 1,
+  author: "Prince Sanel",
+  async execute(senderId, args, pageAccessToken) {
+    const text = args.join(" ");
+    if (!text) {
+      await sendMessage(senderId, {
+        text: `Please Provide a Question.`
+      }, pageAccessToken);
+      return;
+    }
+    try {
+      const res = await axios.get(`https://kaiz-apis.gleeze.com/api/blackbox?ask=${encodeURIComponent(prompt)}&apikey=f05ad551-e7d7-459b-8b27-54e76da15011&uid=100088309851834`);
+      const { response } = res.data;
+
+      if (response) {
+        await sendMessage(senderId, {
+          text: response
+        }, pageAccessToken);
+      } else {
+        throw new Error("There's an error with Api");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      await sendMessage(senderId, {
+        text: `there's an error.`
+      }, pageAccessToken);
+    }
+  }
+};
